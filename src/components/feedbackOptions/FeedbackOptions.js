8afbd22f330c4d73420a434408d9e5b7452fbf23
ka2nq1 <ka2nq1@gmail.com>
2021-01-16 21:03:26 +0200

    hw

--- src/components/feedbackOptions/FeedbackOptions.js
+++ src/components/feedbackOptions/FeedbackOptions.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default function FeedbackOptions({good, neutral, bad}) {
         return (
         <div>
-            Buttons
             <button type='increment' onClick={good}>
                 Good</button>
             <button type='increment' onClick={neutral}>
